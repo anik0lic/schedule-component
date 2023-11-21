1c2b5ad691f56ccd32f88c3f5eb49e15946a76d3
Luka <lmarkovic14422rn@raf.rs>
2023-11-21 15:00:24 +0100

    gagi

--- schedule-test/src/main/java/raf/sk/projekat1/gui/InfoJSON.java
+++ schedule-test/src/main/java/raf/sk/projekat1/gui/InfoJSON.java
@@ -5,11 +5,90 @@ import java.awt.*;
 
 public class InfoJSON extends JFrame {
 
+    JLabel dayFLabel = new JLabel("Day Format");
+    JLabel dateFLabel = new JLabel("Date Format");
+    JLabel fExpLabel = new JLabel("File");
+    JTextField dayFTF = new JTextField(20);
+    JTextField dateFTF = new JTextField(20);
+
+
+    JFileChooser jfc = new JFileChooser();
+
+    private JButton dugmeJfc = new JButton("Select a file");
+    private JButton dugme = new JButton("Next");
+
     public InfoJSON(Frame owner) throws HeadlessException {
 
-        setSize(700, 500);
+
+
+        dugmeJfc.setBackground(Color.CYAN);
+
+        dugme.setBackground(Color.CYAN);
+
+
+
+//        JFrame frame = new JFrame("Json Presets");
+        setTitle("Json Presets");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(owner);
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        setSize((int) (screenWidth / 4), (int) (screenHeight / 4));
+        setLocationRelativeTo(null);
+
+        JPanel newPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+
+        constraints.insets = new Insets(10, 0, 0, 10);
+
+
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        newPanel.add(dayFLabel, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(dayFTF, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        newPanel.add(dateFLabel, constraints);
+
+        constraints.gridx = 1;
+        newPanel.add(dateFTF, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        newPanel.add(fExpLabel, constraints);
+
+        constraints.gridx = 1;
+
+        newPanel.add(dugmeJfc, constraints);
+
+
+
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.EAST;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.insets = new Insets(70, 0, 0, 10);
+        newPanel.add(dugme, constraints);
+
+
+
+
+
+        add(newPanel);
+
+
 
     }
 }
